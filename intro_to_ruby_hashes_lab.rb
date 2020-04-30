07aeba842c010492cbd82fd1227e9ba896370c06
Jung Lee <lee.jung.s95@gmail.com>
2020-04-30 04:39:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {4}}
+	monopoly = {:railroads => {}}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
